12466ee00c69009f49775e563076ab73e7b9c001
pasivad <pasichnyktwitch@gmail.com>
2026-04-25 02:27:45 +0300

    fix: base UI

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,15 @@ import Image from 'next/image';
 
 export default function Home() {
   return (
-    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      Vlad Pasichnyk's Portfolio
+    <div className="flex flex-col flex-1 items-center justify-center font-sans text-2xl font-bold">
+      Vlad Pasichnyk's Portfolio, still in development. Please check back later.
+      <Image
+        src="/construction.avif"
+        alt="Under Construction"
+        width={700}
+        height={500}
+        className="mt-8"
+      />
     </div>
   );
 }
